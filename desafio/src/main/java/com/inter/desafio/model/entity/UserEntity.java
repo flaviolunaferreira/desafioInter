82ac6c1f52cf29058b1c538ee09c9bc46910a5fe
theCoyote <flaviolunaferreira@gmail.com>
2024-06-17 13:01:54 -0300

    implementando classe de usuario

---
+++ desafio/src/main/java/com/inter/desafio/model/entity/UserEntity.java
@@ -0,0 +1,22 @@
+package com.inter.desafio.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+public class UserEntity extends BasicEntity {
+
+    private String nome;
+    private String email;
+    private String senha;
+    private String cpfCnpj;
+    
+}
